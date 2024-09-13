e796c08917173ab17c91f87fc876aad79bb09875
Oscar van der Westhuixen <oscar.vdw99@gmail.com>
2024-09-13 15:49:20 +0200

    Continue with clearBrands, clear range.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const brand = 'Direct Line'; //Set this value in main, once we get there.
+
 var index = new Map([
   ['Created At',-1],
   ['Completed At',-1],
@@ -27,23 +29,91 @@ function getData() {
 
 function setDates() {
   var playdate = new Date();
-  dates.END.setMonth(dates.get('end').getMonth()-1);
+  dates.END.setMonth(dates.END.getMonth()-1);
   dates.END.setDate(1);
   dates.END.setHours(0);
 
-  dates.SIX.setMonth(dates.get('six').getMonth()-7);
+  dates.SIX.setMonth(dates.SIX.getMonth()-7);
   dates.SIX.setDate(1);
   dates.SIX.setHours(0);
 
-  dates.TWELVE.setMonth(dates.get('twelve').getMonth()-13);
+  dates.TWELVE.setMonth(dates.TWELVE.getMonth()-13);
   dates.TWELVE.setDate(1);
   dates.TWELVE.setHours(0);
 
   Logger.log(dates.get('end'));
 }
 
+function setIndex() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('AppScript');
+  var headers = sheet.getRange(1,1,1,sheet.getLastColumn()).getValues()[0];
+  for(let i=0; i<headers.length; i++) {
+    index.forEach(function(value, key){
+      if(headers[i] == key) {
+        index.set(key, i+1);
+      }
+    })
+  }
+  
+  index.forEach(function(value, key){
+    if(headers[value]<0) {
+      if(sheet.getLastColumn() == sheet.getMaxColumns()) {
+        sheet.insertColumnsAfter(sheet.getLastColumn());
+      }
+      sheet.getRange(1,sheet.getLastColumn()+1).setValue(key);
+      index.set(key, sheet.getLastColumn())
+    }
+  })
+
+  /*index.forEach(function(value, key) {
+    Logger.log(key+' + '+value)
+    Logger.log(key + ' : ' + sheet.getRange(1,value).getA1Notation());
+  })*/
+}
+
+function clearBrands() {
+  setIndex();
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('AppScript');
+  var range = sheet.getRange(1,1,sheet.getLastRow(),sheet.getLastColumn());
+  var filter = sheet.getFilter();
+  if(filter) {
+    filter.remove();
+  }
+  range.createFilter();
+  sheet.sort(index.get('All Brands'),true);
+  sheet.getFilter().remove();
+  var startingFrom = -1;
+  var upUntil = -1;
+  var cellValue = '';
+
+  for(var i=2; i<sheet.getLastRow(); i++) {
+    cellValue = sheet.getRange(i,index.get('All Brands')).getValue();
+    if(cellValue == brand) {
+      startingFrom = i;
+      Logger.log(startingFrom);
+      break;
+    }
+  }
+
+  for(var i=startingFrom; i<sheet.getLastRow(); i++) {
+    cellValue = sheet.getRange(i,index.get('All Brands')).getValue();
+    if(cellValue != brand) {
+      upUntil = i;
+      Logger.log(upUntil);
+      break;
+    }
+  }
+  
+  //sheet.deleteRows(startingFrom,upUntil-startingFrom);
+  //The range, starting -> from up until must be retained. Thus, if
+  if(startingFrom<=1) {
+    sheet.getRange(1,startingFrom,startingFrom-2).clearContent();
+  } else {
+    sheet
+  }
+}
+
 /**
- * setDatesConfig : start and end dates - month end, 6 months - and 12 months ago.
  * clearBrands : removes non-DL / non-CHC brands (Blanks?)
  * clearGrumbles : removes all grumbles (Blanks?)
  * clearOldComplaints : removes all complaints that are not within the current reporting period
